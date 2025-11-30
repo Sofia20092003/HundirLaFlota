dd2d5917bb333042e74d4669a59906d99c6bbbe2
Sofia20092003 <angelasofiarodriguezarcinegas03@liceolapaz.net>
2025-11-30 23:33:47 +0100

    Avanzando Clase Barcos

--- src/Barcos.java
+++ src/Barcos.java
@@ -1,5 +1,29 @@
 public class Barcos {
     public static void colocarBarcosAleatorios(int[][] tableroBarcos, int [] tamanoBarcos){
-        
+        for (int id=0; id<tamanoBarcos.length; id++){
+            int tam=tamanoBarcos[id];
+            boolean colocado=false;
+
+            while (!colocado){
+                int fila= Utilidades.numeroAleatorio(0, tableroBarcos.length-1);
+                int col= Utilidades.numeroAleatorio(0, tableroBarcos[0].length-1);
+                boolean horizontal= Utilidades.numeroAleatorio(0,1)==0;
+
+                if(sePuedecolocarBarco(tableroBarcos, fila, col, tam, horizontal)){
+                    colocarBarco(tableroBarcos, fila, col, tam, horizontal, id);
+                    colocado=true;
+                }
+            }
+        }
+    }
+    public static boolean sePuedecolocarBarco(int[][] tablero, int fila, int columna, int tamano, boolean horizontal){
+        int filas=tablero.length;
+        int columnas=tablero[0].length;
+
+        if(horizontal){
+            if(columna+tamano>columnas){
+                return false;
+            }
+        }
     }
 }
