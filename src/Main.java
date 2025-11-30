b87b625078adc596b3cebfe56e0df622f5c27b65
Sofia20092003 <angelasofiarodriguezarcinegas03@liceolapaz.net>
2025-11-30 18:57:44 +0100

    Iniciando Practica Final Hundir la Flota

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static final int FILAS=10;
+    public static final int COLUMNAS=10;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] tamanosBarcos={5, 4, 3, 3, 2};
+        int numBarcos= tamanosBarcos.length;
+
+        int [][] tableroBarcosJugador=
+    }
+}
