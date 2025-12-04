bc391b4e185188b3d7999f2ab7c508408c4fc2da
Sofia20092003 <angelasofiarodriguezarcinegas03@liceolapaz.net>
2025-12-04 18:35:47 +0100

    Avanzando Clase Utilidades

--- src/Utilidades.java
+++ src/Utilidades.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class Utilidades {
-    
+    private static final Scanner SC = new Scanner(System.in);
+
+    public static int numeroAleatorio(int min, int max){
+        return (int)(Math.random()*(max-min+1))+min;
+    }
+
+    public static int convertirFila(String coord){
+        if(coord.length()<2){
+            return -1;
+        }
+        String numero=coord.substring(1);
+        try{
+            return Integer.parseInt(numero);
+        }catch (Exception e){
+            return -1;
+        }
+    }
 }
