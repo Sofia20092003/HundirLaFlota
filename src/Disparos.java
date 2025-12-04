99276cb1f8d259b990d28300b8b4f5f38c4a7b6b
Sofia20092003 <angelasofiarodriguezarcinegas03@liceolapaz.net>
2025-12-04 18:23:14 +0100

    Avanzando Clase Disparos

--- src/Disparos.java
+++ src/Disparos.java
@@ -1,3 +1,19 @@
 public class Disparos {
-    
+    public static boolean yaDisparados(char[][] tableroDisparos, int fila, int columna){
+        return tableroDisparos[fila][columna]!='~';
+    }
+    public static boolean procesarDisparo(
+            int fila,
+            int columna,
+            int [][] tableroBarcos,
+            char[][] tableroDisparos,
+            int[] impactosBarcos,
+            int[] tamanoBarcos
+    ){
+        int idBarco= tableroBarcos[fila][columna];
+        if(idBarco==-1){
+            tableroDisparos[fila][columna]='A';
+            return false;
+        }
+    }
 }
